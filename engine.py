30ae8ecc783e50230506c236a59fd797c41a3856
Alexlived <thelegendaryrahin@gmail.com>
2026-06-11 15:52:37 +0600

    engine is finished,time for the simulation engine is finished i added barca and madrid players, and now im gonna do the 90 min loop

---
+++ engine.py
@@ -0,0 +1,160 @@
+import random
+
+position_groups = {
+    "attacking": ["RW", "LW", "ST", "CAM"],
+    "defensive": ["CB", "CDM", "LB", "RB"],
+    "Mid": ["CM","CDM","CAM"]
+}
+class Player:
+    def __init__(self,n,p,pac,dfn,atk,pas,sht,gk,phy):
+        self.name = n
+        self.position = p
+
+        # Base stats
+        self.pac = pac
+        self.dfn = dfn
+        self.atk = atk
+        self.pas = pas
+        self.sht = sht
+        self.phy = phy
+        self.gk = gk
+
+        pass
+
+        self.stamina = 100
+
+    def get_effective_stat(self, stat_name):
+        # Calculates stat penalty based on current stamina.
+        base_stat = getattr(self, stat_name, 50)
+        stamina_factor = max(0.2, self.stamina / 100.0) # Floor at 20% performance
+        return int(base_stat * stamina_factor)
+
+    def decay(self,amount): # decay stamina
+        self.stamina = max(10, self.stamina - amount)
+    
+    def recover(self,amount): #recover in breaks
+        self.stamina = min(100, self.stamina + amount)
+
+
+class Team:
+    def __init__(self,name,players,state,profile):
+        self.name = name
+        self.players = players
+        self.state = state
+        self.profile = profile
+
+    def decay(self):
+        for player in self.players:
+            player.decay(random.randint(3, 10)) # Decay each player's stamina by a random amount of points per duel
+    
+
+       
+        
+
+class Part_of_Pitch:   
+    def __init__(self,zone,possesion,team_1,team_2):
+        self.zone = zone
+        self.team_1 = team_1
+        self.team_2 = team_2
+
+    def update_zone(self, duel_outcome):
+        if duel_outcome in self.team_1.players:
+            self.zone += 1
+            pass
+        else:
+            self.zone -= 1
+            pass
+\
+
+
+def get_players_by_zone(team_1, team_2, zone):
+    # For central zones, we want to select midfielders from both teams
+    if 0 <= zone <= 2:
+        relevant_team_1_players = [player for player in team_1.players if player.position in position_groups["Mid"]]
+        relevant_team_2_players = [player for player in team_2.players if player.position in position_groups["Mid"]]
+    elif -2 <= zone <= 0:
+        relevant_team_1_players = [player for player in team_1.players if player.position in position_groups["Mid"]]
+        relevant_team_2_players = [player for player in team_2.players if player.position in position_groups["Mid"]]
+
+    # For attacking zones, we want to select attackers from the attacking team and defenders from the defending team
+    elif zone >= 3:
+        relevant_team_1_players = [player for player in team_1.players if player.position in position_groups["attacking"]]
+        relevant_team_2_players = [player for player in team_2.players if player.position in position_groups["defensive"]]
+    else: # zone <= -3
+        relevant_team_1_players = [player for player in team_1.players if player.position in position_groups["defensive"]]
+        relevant_team_2_players = [player for player in team_2.players if player.position in position_groups["attacking"]]
+    return relevant_team_1_players, relevant_team_2_players
+
+def get_random_duelists(team_1, team_2, zone):
+    relevant_team_1_players, relevant_team_2_players = get_players_by_zone(team_1, team_2, zone) # Using the function
+    attacker = random.choice(relevant_team_1_players)
+    defender = random.choice(relevant_team_2_players)
+    return attacker, defender
+
+def team_profile_modifier(player, profile):
+    atk_stats = ["atk","pac","phy", "sht"]
+    mid_stats = ["pas", "pac"]
+    dfn_stats = ["dfn", "pac","phy"]
+
+
+    if profile == "Attacking":
+        for stat in atk_stats:
+            setattr(player, stat, getattr(player, stat)*1.4)
+        for stat in mid_stats:
+            setattr(player, stat, getattr(player, stat)*1.2)
+        for stat in dfn_stats:
+            setattr(player, stat, getattr(player, stat)*0.6)
+    elif profile == "Defensive":
+        for stat in atk_stats:
+            setattr(player, stat, getattr(player, stat)*0.6)
+        for stat in mid_stats:
+            setattr(player, stat, getattr(player, stat)*1.2)
+        for stat in dfn_stats:
+            setattr(player, stat, getattr(player, stat)*1.4)
+    else: # Balanced
+        for stat in atk_stats:
+            setattr(player, stat, getattr(player, stat)*1.2)
+        for stat in mid_stats:
+            setattr(player, stat, getattr(player, stat)*1.2)
+        for stat in dfn_stats:
+            setattr(player, stat, getattr(player, stat)*1.2)
+
+    
+
+        
+        
+
+def simulate_duel(attacker, defender, team_attacking, team_defending,zone):
+    if 0 <= zone <= 2:
+        attacker_stat = attacker.get_effective_stat("pas") + attacker.get_effective_stat("atk") # Midfield duels rely on passing and intelligence
+        defender_stat = defender.get_effective_stat("pas") + defender.get_effective_stat("dfn") 
+    elif -2 <= zone <= 0:
+        attacker_stat = attacker.get_effective_stat("pas") + attacker.get_effective_stat("dfn") 
+        defender_stat = defender.get_effective_stat("pas") + defender.get_effective_stat("atk")
+
+    
+    elif zone >= 3:
+        attacker_stat = attacker.get_effective_stat("atk") + attacker.get_effective_stat("pac") + attacker.get_effective_stat("phy") 
+        defender_stat = defender.get_effective_stat("dfn") + defender.get_effective_stat("pac") + defender.get_effective_stat("phy") 
+    elif zone <= -3:
+        attacker_stat = attacker.get_effective_stat("dfn") + attacker.get_effective_stat("pac") + attacker.get_effective_stat("phy") 
+        defender_stat = defender.get_effective_stat("atk") + defender.get_effective_stat("pac") + defender.get_effective_stat("phy")
+
+    winners = [attacker, defender]
+    weights = [attacker_stat, defender_stat]
+    random_winner = random.choices(winners, weights=weights, k=1)[0]
+    return random_winner
+
+def simulate_shot(attacker, goalkeeper):
+    shot_stat = attacker.get_effective_stat("sht")
+    gk_stat = goalkeeper.get_effective_stat("gk")
+    winners = [attacker, goalkeeper]
+    weights = [shot_stat, gk_stat]
+    random_winner = random.choices(winners, weights=weights, k=1)[0]
+    if random_winner == attacker:
+        return 'Goal'
+    else:
+        return 'Saved'
+
+def main():
+    
--- main.py
+++ main.py
@@ -1,140 +1,50 @@
-import random
+import time
+from engine import Player, Team, Part_of_Pitch, team_profile_modifier, get_random_duelists, simulate_duel, simulate_shot
 
-position_groups = {
-    "attacking": ["RW", "LW", "ST", "CAM"],
-    "defensive": ["CB", "CDM", "LB", "RB"],
-    "Mid": ["CM","CDM","CAM"]
-}
-class Player:
-    def __init__(self,n,p,pac,dfn,atk,pas,sht,gk,phy,team):
-        self.name = n
-        self.position = p
+def main():
+    # Real Madrid Player Array
+    rm_players = [
+        Player("Courtois", "gk", 45, 15, 10, 75, 10, 78, 90),
+        Player("Carvajal", "RB", 80, 85, 78, 78, 54, 81, 10),
+        Player("Rüdiger", "CB", 82, 87, 55, 71, 53, 86, 10),
+        Player("Militao", "CB", 85, 85, 58, 70, 50, 82, 10),
+        Player("Mendy", "LB", 89, 80, 75, 74, 62, 84, 10),
+        Player("Tchouameni", "CDM", 72, 83, 74, 79, 69, 84, 10),
+        Player("Valverde", "CM", 88, 80, 84, 85, 82, 85, 10),
+        Player("Bellingham", "CAM", 80, 78, 87, 85, 85, 83, 10),
+        Player("Rodrygo", "RW", 88, 42, 86, 80, 82, 70, 10),
+        Player("Mbappe", "ST", 97, 36, 93, 80, 90, 78, 10),
+        Player("Vinicius", "LW", 95, 30, 91, 81, 84, 68, 10)
+    ]
 
-        # Base stats
-        self.pac = pac
-        self.dfn = dfn
-        self.atk = atk
-        self.pas = pas
-        self.sht = sht
-        self.phy = phy
-        self.gk = gk
+    # Barcelona Player Array
+    barca_players = [
+        Player("Ter Stegen", "gk", 40, 10, 12, 85, 10, 76, 89),
+        Player("Kounde", "RB", 80, 85, 70, 75, 45, 78, 10),
+        Player("Araujo", "CB", 83, 86, 50, 65, 48, 85, 10),
+        Player("Cubarsi", "CB", 72, 81, 62, 80, 38, 73, 10),
+        Player("Balde", "LB", 91, 75, 78, 73, 48, 70, 10),
+        Player("Casado", "CDM", 75, 78, 72, 81, 60, 76, 10),
+        Player("De Jong", "CM", 82, 77, 83, 86, 69, 78, 10),
+        Player("Pedri", "CAM", 78, 68, 86, 87, 70, 70, 10),
+        Player("Yamal", "RW", 85, 35, 84, 81, 78, 65, 10),
+        Player("Lewandowski", "ST", 75, 44, 88, 70, 88, 80, 10),
+        Player("Raphinha", "LW", 89, 50, 85, 82, 80, 73, 10)
+    ]
 
-        self.team = team
-        pass
+    # 3. Team objects
+    real_madrid = Team(name="Real Madrid", players=rm_players, state="defending", profile="Balanced")
+    barcelona = Team(name="Barcelona", players=barca_players, state="attacking", profile="Attacking")
 
-        self.stamina = 100
-
-    def get_effective_stat(self, stat_name):
-        # Calculates stat penalty based on current stamina.
-        base_stat = getattr(self, stat_name, 50)
-        stamina_factor = max(0.2, self.stamina / 100.0) # Floor at 20% performance
-        return int(base_stat * stamina_factor)
-
-    def decay(self,amount): # decay stamina
-        self.stamina = max(10, self.stamina - amount)
-    
-    def recover(self,amount): #recover in breaks
-        self.stamina = min(100, self.stamina + amount)
-
-
-class Team:
-    def __init__(self,name,players,state,profile):
-        self.name = name
-        self.players = players
-        self.state = state
-        self.profile = profile
-
-    def decay(self):
-        for player in self.players:
-            player.decay(random.randint(3, 10)) # Decay each player's stamina by a random amount of points per duel
-    
-    def calc_modifier(self, stat_name, player):
-        profiles = ["Attacking", "Defensive", "Balanced"]
-        if self.profile == "Attacking":
-            if stat_name in ["atk", "sht", "phy","pac"]:
-                atk_amount = 1.4
-                pass
-            elif stat_name in ["pas", "pac"]:
-                mid_amount = 1.2
-            if stat_name in ["dfn", "pac", "phy"]:
-                def_amount = 0.6
-        elif self.profile == "Defensive":
-            if stat_name in ["dfn", "pac","phy"]:
-                def_amount = 1.4
-            if stat_name in ["pas", "pac"]:
-                mid_amount = 1.0
-            if stat_name in ["atk","pac","phy", "sht"]:
-                atk_amount = 0.8
-        else: # Balanced
-            if stat_name in ["pas", "pac"]:
-                mid_amount = 1.2
-            if stat_name in ["atk", "sht","phy", "pac"]:
-                atk_amount = 1.2
-            if stat_name in ["dfn", "pac","phy"]:
-                def_amount = 1.0
+    # 4. Apply One-Time Pre-Match Tactical Profile Multipliers
+    for player in real_madrid.players:
+        team_profile_modifier(player, real_madrid.profile)
         
-       
-        
+    for player in barcelona.players:
+        team_profile_modifier(player, barcelona.profile)
 
-class Part_of_Pitch:   
-    def __init__(self,zone,possesion,team_1,team_2):
-        self.zone = zone
-        self.team_1 = team_1
-        self.team_2 = team_2
+    # 5. Connect Teams to the Global Pitch Simulation Manager
+    pitch = Part_of_Pitch(zone=0, possesion=real_madrid, team_1=real_madrid, team_2=barcelona)
 
-    def update_zone(self, duel_outcome):
-        if duel_outcome in self.team_1.players:
-            self.zone += 1
-        else:
-            self.zone -= 1
-        if self.zone == 5:
-            pass # Team 1 scores
-        elif self.zone == -5:
-            pass # Team 2 scores
-
-def get_players_by_zone(team_1, team_2, zone):
-    # For central zones, we want to select midfielders from both teams
-    if 0 <= zone <= 2:
-        relevant_team_1_players = [player for player in team_1.players if player.position in position_groups["Mid"]]
-        relevant_team_2_players = [player for player in team_2.players if player.position in position_groups["Mid"]]
-    elif -2 <= zone <= 0:
-        relevant_team_1_players = [player for player in team_1.players if player.position in position_groups["Mid"]]
-        relevant_team_2_players = [player for player in team_2.players if player.position in position_groups["Mid"]]
-
-    # For attacking zones, we want to select attackers from the attacking team and defenders from the defending team
-    elif zone >= 3:
-        relevant_team_1_players = [player for player in team_1.players if player.position in position_groups["attacking"]]
-        relevant_team_2_players = [player for player in team_2.players if player.position in position_groups["defensive"]]
-    else: # zone <= -3
-        relevant_team_1_players = [player for player in team_1.players if player.position in position_groups["defensive"]]
-        relevant_team_2_players = [player for player in team_2.players if player.position in position_groups["attacking"]]
-    return relevant_team_1_players, relevant_team_2_players
-
-def get_random_duelists(team_1, team_2, zone):
-    relevant_team_1_players, relevant_team_2_players = get_players_by_zone(team_1, team_2, zone) # Using the function
-    attacker = random.choice(relevant_team_1_players)
-    defender = random.choice(relevant_team_2_players)
-    return attacker, defender
-
-def team_profile_modifier(player):
-    if player.team.profile == "Attacking":
-        
-        
-
-def simulate_duel(attacker, defender, team_attacking, team_defending,zone):
-    if 0 <= zone <= 2:
-        attacker_stat = attacker.get_effective_stat("pas") + attacker.get_effective_stat("atk") # Midfield duels rely on passing and intelligence
-        defender_stat = defender.get_effective_stat("pas") + defender.get_effective_stat("dfn") 
-    elif -2 <= zone <= 0:
-        attacker_stat = attacker.get_effective_stat("pas") + attacker.get_effective_stat("dfn") 
-        defender_stat = defender.get_effective_stat("pas") + defender.get_effective_stat("atk")
-
-    
-    elif zone >= 3:
-        attacker_stat = attacker.get_effective_stat("atk") + attacker.get_effective_stat("pac") + attacker.get_effective_stat("phy") 
-        defender_stat = defender.get_effective_stat("dfn") + defender.get_effective_stat("pac") + defender.get_effective_stat("phy") 
-    elif zone <= -3:
-        attacker_stat = attacker.get_effective_stat("dfn") + attacker.get_effective_stat("pac") + attacker.get_effective_stat("phy") 
-        defender_stat = defender.get_effective_stat("atk") + defender.get_effective_stat("pac") + defender.get_effective_stat("phy")
-
-    # Apply team profile modifiers
+    # Score Trackers
+    score = {real_madrid.name: 0, barcelona.name: 0}
